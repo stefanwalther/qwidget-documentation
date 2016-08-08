9e0e1ee42c9f9b06ca780fddf9227295dc2b9136
Stefan Walther <Stefan.Walther@qlik.com>
2016-08-08 22:34:25 +0200

    Updates

---
+++ views/assets/js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $('#toc').tocify(
+        {
+            selectors: 'h1,h2,h3',
+            theme: 'bootstrap',
+            scrollTo: 100,
+            extendPage: false
+            //,
+            //showAndHide: false
+        });
+
+});
+
